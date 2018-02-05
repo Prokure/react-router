488257b7bb76300b5da2d7e027bc0818bf322aa9
shubham-agrawal <shubham@prokure.it>
2018-02-05 18:07:58 +0530

    minor updates

--- packages/react-router-dom/modules/Router.js
+++ packages/react-router-dom/modules/Router.js
@@ -1,3 +1,3 @@
 // Written in this round about way for babel-transform-imports
-import { Router } from "react-router";
+import { Router } from "react-horizontal-router-temp";
 export default Router;
--- packages/react-router-dom/modules/matchPath.js
+++ packages/react-router-dom/modules/matchPath.js
@@ -1,3 +1,3 @@
 // Written in this round about way for babel-transform-imports
-import { matchPath } from "react-router";
+import { matchPath } from "react-horizontal-router-temp";
 export default matchPath;
